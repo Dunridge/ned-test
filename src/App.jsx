9ebb874027dc76a163e6628b321f45a2d7971747
Dunridge <Polinka7max@gmail.com>
2023-05-01 21:57:06 -0700

    develop. add the form section

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,43 @@ import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [configuration, setConfiguration] = useState({});
+    const [configuration, setConfiguration] = useState({});
 
-  // fetch data from https://raw.githubusercontent.com/Ned-Helps/demo-repository/main/config.json
+    useEffect(() => {
+        // some malfunction with the endpoint
+        fetch(
+            "https://raw.githubusercontent.com/Ned-Helps/demo-repository/main/config.json"
+        )
+            .then((response) => response.json())
+            .then((configuration) => {
+                // console.log('configuration', configuration);
+                let formattedConfiguration = {
+                    desiredFeePercentage: configuration[0],
+                    desiredRepaymentDelay: configuration[1],
+                    fundingAmount: configuration[2],
+                    revenueAmount: configuration[3],
+                    revenuePercentage: configuration[4],
+                    revenueSharedFrequency: configuration[5],
+                    useOfFunds: configuration[6],
+                    fundingAmountMax: configuration[7],
+                    fundingAmountMin: configuration[8],
+                    revenuePercentageMin: configuration[9],
+                    revenuePercentageMax: configuration[10],
+                };
 
-  useEffect(() => {
-    fetch('https://raw.githubusercontent.com/Ned-Helps/demo-repository/main/config.json')
-      .then((response) => response.json())
-      .then((configuration) => {
-        // console.log('configuration', configuration);
-        // console.log('field', configuration[0]);
+                setConfiguration(formattedConfiguration);
+            })
+            .catch((error) => console.error(error));
+    }, []);
 
-        let formattedConfiguration = {
-          desiredFeePercentage: configuration[0],
-          desiredRepaymentDelay: configuration[1],
-          fundingAmount: configuration[2],
-          revenueAmount: configuration[3],
-          revenuePercentage: configuration[4],
-          revenueSharedFrequency: configuration[5],
-          useOfFunds: configuration[6],
-          fundingAmountMax: configuration[7],
-          fundingAmountMin: configuration[8],
-          revenuePercentageMin: configuration[9],
-          revenuePercentageMax: configuration[10]
-        };
-
-        console.log('formattedConfiguration: ', formattedConfiguration);
-
-        // setConfiguration(configuration)
-        setConfiguration(formattedConfiguration);
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
-
-  return (
-    <div className="app">
-      <Header></Header>
-      {/* <div className="app__body"> */}
-        <FinancingOptions configuration={configuration}></FinancingOptions>
-      {/* </div> */}
-      {/* TODO: or add the footer buttons to the app to avoid complexity */}
-      <Footer></Footer>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header></Header>
+            <FinancingOptions configuration={configuration}></FinancingOptions>
+            {/* TODO: or add the footer buttons to the app to avoid complexity */}
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
--- src/components/FinancingOptions.jsx
+++ src/components/FinancingOptions.jsx
@@ -1,88 +1,166 @@
 import { useState } from "react";
 
+// the endpoint returns an error - failed to fetch
 const FinancingOptions = ({ configuration }) => {
-    console.log('configuration', configuration);
+    console.log("configuration at input", configuration); // works
     const [config, setConfig] = useState(configuration);
-    // TODO: use the input for the values 
-    // TODO: solve the issue here
-    // const [revenue, setRevenue] = useState(configuration?.revenueAmount?.placeholder);
-    const [revenue, setRevenue] = useState("0");
-    const [desiredLoan, setDesiredLoan] = useState("0");
-    const [revenueShare, setRevenueShare] = useState("0");
-    const [revenueFrequency, setRevenueFrequency] = useState("0");
-    const [repaymentDelay, setRepaymentDelay] = useState("0");
-    const [fundsPurpose, setFundsPurpose] = useState("0");
+    const [revenue, setRevenue] = useState(
+        config?.revenueAmount?.value || "400"
+    );
+    const [desiredLoan, setDesiredLoan] = useState(
+        config?.fundingAmountMin?.value || "0"
+    );
 
+    const [revenueShare, setRevenueShare] = useState(
+        config?.revenuePercentage?.value || "7.56%"
+    );
+
+    const [revenueFrequency, setRevenueFrequency] = useState(
+        config?.revenueSharedFrequency?.value || "7.56%"
+    );
+
+    const [repaymentDelay, setRepaymentDelay] = useState(
+        config?.desiredRepaymentDelay?.value || "7.56%"
+    );
+
+    const [fundsPurpose, setFundsPurpose] = useState(
+        config?.useOfFunds?.value || "7.56%"
+    );
+
+    const [selectedFrequencyOption, setSelectedFrequencyOption] = useState(
+        "Weekly"
+    );
+
+    const [selectedPaymentDelay, setSelectedPaymentDelay] = useState();
+
+    // TODO: WIP
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(configuration);
+        // console.log(configuration);
+        console.log("config", config);
         // TODO: define the submit if the logic demands it (for now the data is reflected on the UI at once)
     };
+
+    const handleFrequencyOptionChange = (event) => {
+        setSelectedFrequencyOption(event.target.value);
+        // setRepaymentDelay(event.target.value);
+    };
+
+    const handlePaymentDelayChange = () => {
+        // TODO: continue defining the payment delay change
+    }
 
     return (
         <div className="options">
             <form className="options__form" onSubmit={handleSubmit}>
                 {/* TODO: put these fields into components */}
+
+                {/* annual business revenue */}
                 <label className="options__field">
                     <span>What is your annual business revenue?</span>
                     {/* TODO: make it a field that has to be filled out */}
                     <input
-                        type="text"
+                        type="number"
                         value={revenue}
                         onChange={(event) => {
-                            console.log(config);
+                            console.log("config", config);
                             setRevenue(event.target.value);
                         }}
                     />
                 </label>
 
+                {/* desired loan amount */}
                 <label className="options__field">
                     <span>What is your desired loan amount?</span>
                     {/* TODO: make it a field that has to be filled out */}
                     <input
-                        type="text"
+                        type="range"
                         value={desiredLoan}
+                        min={config?.fundingAmountMin?.value || 0}
+                        max={config?.fundingAmountMax?.value || 100}
                         onChange={(event) => setDesiredLoan(event.target.value)}
                     />
                 </label>
 
+                {/* revenue share percentage */}
                 <label className="options__field">
-                    <span>Revenue share percentage:</span>
+                    <span>
+                        Revenue share percentage:{" "}
+                        {config?.revenuePercentage || "70%"}
+                    </span>
                     {/* TODO: make it a field that has to be filled out */}
-                    <input
+                    {/* <input
                         type="text"
                         value={revenueShare}
                         onChange={(event) => setRevenueShare(event.target.value)}
-                    />
+                    /> */}
                 </label>
 
+                {/* Revenue shared frequency */}
                 <label className="options__field">
                     <span>Revenue shared frequency</span>
-                    {/* TODO: make it a field that has to be filled out */}
-                    <input
-                        type="text"
-                        value={revenueFrequency}
-                        onChange={(event) => setRevenueFrequency(event.target.value)}
-                    />
+
+                    <div className="options__frequencies">
+                        <div>
+                            <input
+                                type="radio"
+                                id="option1"
+                                name="options"
+                                value="Monthly"
+                                checked={selectedFrequencyOption === "Monthly"}
+                                onChange={handleFrequencyOptionChange}
+                            />
+                            <label htmlFor="option1">Monthly</label>
+                        </div>
+                        <div>
+                            <input
+                                type="radio"
+                                id="option2"
+                                name="options"
+                                value="option2"
+                                checked={selectedFrequencyOption === "option2"}
+                                onChange={handleFrequencyOptionChange}
+                            />
+                            <label htmlFor="option2">Weekly</label>
+                        </div>
+                    </div>
                 </label>
 
+                {/* Desired payment delay         */}
                 <label className="options__field">
                     <span>Desired payment delay</span>
                     {/* TODO: make it a field that has to be filled out */}
-                    <input
+                    <select 
+                        id="options" 
+                        value={repaymentDelay}
+                        onChange={(event) =>
+                            setRepaymentDelay(event.target.value)
+                        }
+                        >
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                    </select>
+                    
+                    {/* <input
                         type="text"
                         value={repaymentDelay}
-                        onChange={(event) => setRepaymentDelay(event.target.value)}
-                    />
+                        onChange={(event) =>
+                            setRepaymentDelay(event.target.value)
+                        }
+                    /> */}
                 </label>
 
+                {/* funds for */}
                 <label className="options__field">
                     <span>What will you use the funds for?</span>
                     {/* TODO: make it a field that has to be filled out */}
                     <input
                         type="text"
                         value={fundsPurpose}
-                        onChange={(event) => setFundsPurpose(event.target.value)}
+                        onChange={(event) =>
+                            setFundsPurpose(event.target.value)
+                        }
                     />
                 </label>
 
