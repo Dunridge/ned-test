497b88c7aef02d5ea4bf803f93ed8271e9ccf126
Dunridge <Polinka7max@gmail.com>
2023-05-01 17:43:03 -0700

    develop. add the base fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Header from "./components/Header";
 import FinancingOptions from "./components/FinancingOptions";
-import Results from "./components/Results";
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
+
+  // fetch data from https://raw.githubusercontent.com/Ned-Helps/demo-repository/main/config.json
+
   return (
     <div className="app">
       <Header></Header>
-      <div className="app__body">
+      {/* <div className="app__body"> */}
         <FinancingOptions></FinancingOptions>
-        <Results></Results>
-      </div>
+      {/* </div> */}
+      {/* TODO: or add the footer buttons to the app to avoid complexity */}
       <Footer></Footer>
     </div>
   );
--- src/components/FinancingOptions.jsx
+++ src/components/FinancingOptions.jsx
@@ -1,9 +1,93 @@
+import { useState } from "react";
+import Results from "./Results";
+
 const FinancingOptions = () => {
-    return ( 
+    const [revenue, setRevenue] = useState("0");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // TODO: define the submit if the logic demands it (for now the data is reflected on the UI at once)
+    };
+
+    return (
         <div className="options">
-            Options
+            <form className="options__form" onSubmit={handleSubmit}>
+                {/* TODO: put these fields into components */}
+                <label className="options__field">
+                    <span>What is your annual business revenue?</span>
+                    {/* TODO: make it a field that has to be filled out */}
+                    <input
+                        type="text"
+                        value={revenue}
+                        onChange={(event) => setRevenue(event.target.value)}
+                    />
+                </label>
+
+                <label className="options__field">
+                    <span>What is your desired loan amount?</span>
+                    {/* TODO: make it a field that has to be filled out */}
+                    <input
+                        type="text"
+                        value={revenue}
+                        onChange={(event) => setRevenue(event.target.value)}
+                    />
+                </label>
+
+                <label className="options__field">
+                    <span>Revenue share percentage:</span>
+                    {/* TODO: make it a field that has to be filled out */}
+                    <input
+                        type="text"
+                        value={revenue}
+                        onChange={(event) => setRevenue(event.target.value)}
+                    />
+                </label>
+
+                <label className="options__field">
+                    <span>Revenue shared frequency</span>
+                    {/* TODO: make it a field that has to be filled out */}
+                    <input
+                        type="text"
+                        value={revenue}
+                        onChange={(event) => setRevenue(event.target.value)}
+                    />
+                </label>
+
+                <label className="options__field">
+                    <span>Desired payment delay</span>
+                    {/* TODO: make it a field that has to be filled out */}
+                    <input
+                        type="text"
+                        value={revenue}
+                        onChange={(event) => setRevenue(event.target.value)}
+                    />
+                </label>
+
+                <label className="options__field">
+                    <span>What will you use the funds for?</span>
+                    {/* TODO: make it a field that has to be filled out */}
+                    <input
+                        type="text"
+                        value={revenue}
+                        onChange={(event) => setRevenue(event.target.value)}
+                    />
+                </label>
+            </form>
+
+            <div className="options__results">
+                {/* TODO: put it in the results component afterwards */}
+                {/* <Results></Results> */}
+
+                <div>Anual Business Revenue {revenue}</div>
+                <div>Funding amount {revenue}</div>
+                <div>Fees {revenue}</div>
+                <hr />
+                <div>Total revenue share {revenue}</div>
+                <div>Expected transfers {revenue}</div>
+                <div>Explected completion date {revenue}</div>
+            </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default FinancingOptions;
