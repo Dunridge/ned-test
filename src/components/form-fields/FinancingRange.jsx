fee25aa5d6ee3553397144cc267f3acb28d2b3cd
Dunridge <Polinka7max@gmail.com>
2023-05-02 15:30:25 -0700

    develop. add loan range

--- src/components/form-fields/FinancingRange.jsx
+++ src/components/form-fields/FinancingRange.jsx
@@ -4,24 +4,33 @@ const FinancingRange = ({ label, config, setConfig }) => {
             <label>
                 <span>{label}</span>
                 {/* TODO: make it a field that has to be filled out */}
+                {/* TODO: add the max and the min values here  */}
                 <div className="options__range-section">
-                    <input
-                        type="range"
-                        min={`${config?.funding_amount_min?.value}` || "0"}
-                        max={`${config?.funding_amount_max?.value}` || "1000"}
-                        value={config?.funding_amount?.value || 0} // config.revenueAmount.placeholder
-                        onChange={(event) => {
-                            // console.log("config", config);
-                            // console.log("event.target.value: ", event.target.value);
-                            setConfig({
-                                ...config,
-                                funding_amount: {
-                                    ...config.funding_amount,
-                                    value: event.target.value,
-                                },
-                            });
-                        }}
-                    />
+                    <div className="options__range-input">
+                        <div className="options__range-values">
+                            <div className="options__range-value">{ config?.funding_amount_min?.value }</div>
+                            <div className="options__range-value">{ config?.funding_amount_max?.value }</div>
+                        </div>
+                        <input
+                            type="range"
+                            min={`${config?.funding_amount_min?.value}` || "0"}
+                            max={
+                                `${config?.funding_amount_max?.value}` || "1000"
+                            }
+                            value={config?.funding_amount?.value || 0} // config.revenueAmount.placeholder
+                            onChange={(event) => {
+                                // console.log("config", config);
+                                // console.log("event.target.value: ", event.target.value);
+                                setConfig({
+                                    ...config,
+                                    funding_amount: {
+                                        ...config.funding_amount,
+                                        value: event.target.value,
+                                    },
+                                });
+                            }}
+                        />
+                    </div>
                     <div>{config?.funding_amount?.value || 0}</div>
                 </div>
             </label>
